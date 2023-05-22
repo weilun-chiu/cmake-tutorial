c6e2ac99a2d182037dec23d05ddcf7f297b2a217
weilun-chiu <john245678@gmail.com>
2023-05-21 20:31:31 -0400

    Step2 files

---
+++ Step2_build/MathFunctions/CMakeFiles/MathFunctions.h.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MathFunctions.h.dir/mysqrt.cxx.o"
+  "libMathFunctions.h.pdb"
+  "libMathFunctions.h.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathFunctions.h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
