75e9f019badba42e2e4c162ad5fcc1801ef1e95e
weilun-chiu <john245678@gmail.com>
2023-05-21 20:31:49 -0400

    Completed step2

--- Step2/tutorial.cxx
+++ Step2/tutorial.cxx
@@ -6,8 +6,12 @@
 #include "TutorialConfig.h"
 
 // TODO 11: Only include MathFunctions if USE_MYMATH is defined
+#ifdef USE_MYMATH
+#  include "MathFunctions.h"
+#endif
 
 // TODO 5: Include MathFunctions.h
+// #include "MathFunctions.h"
 
 int main(int argc, char* argv[])
 {
@@ -23,11 +27,16 @@ int main(int argc, char* argv[])
   const double inputValue = std::stod(argv[1]);
 
   // TODO 12: Use mysqrt if USE_MYMATH is defined and sqrt otherwise
-
+#ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+#else
+  const double outputValue = sqrt(inputValue);
+#endif
   // TODO 6: Replace sqrt with mysqrt
+  
 
   // calculate square root
-  const double outputValue = sqrt(inputValue);
+  // const double outputValue = sqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
